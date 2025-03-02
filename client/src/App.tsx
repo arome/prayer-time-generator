ab66fcb8d478b722bc253f7ac49dc733e0534de6
Omar Halbouni <omarhalbouni@gmail.com>
2025-03-02 14:09:44 -0500

    feat: Configure project for deployment and add VSCode settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { UploadCloud, Globe } from "lucide-react";
-import { PrayerTimes, ProcessedData } from "./App.types";
+import { ProcessedData } from "./App.types";
 import { MONTHS, MONTHS_SHORT } from "./App.consts";
 import { downloadData } from "./functions/downloadData";
 import { formatTimeWithAMPM } from "./functions/formatTimeWithAMPM";
